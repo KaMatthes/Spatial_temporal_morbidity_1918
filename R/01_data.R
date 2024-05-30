897fc18608c6688a92e8bddbd316b94a8f611792
Katarina Matthes <katarina.matthes@iem.uzh.ch>
2024-05-30 15:27:28 +0200

    minor changes

--- R/01_data.R
+++ R/01_data.R
@@ -21,10 +21,7 @@ dt.1920 <- read_excel_allsheets("data/SPARK_Digitalisierung_1920.xlsx") %>%
   select(1:8, 10:11)
 
 dt <- rbind(dt.1918,dt.1919,dt.1920) %>%
-  ungroup()
-
-
-dt <- dt %>%
+  ungroup() %>%
   mutate(Startdatum = ymd(Startdatum),
          Enddatum = ymd(Enddatum),
          iso_cw = isoweek(Enddatum),
@@ -38,15 +35,16 @@ dt <- dt %>%
          Gemeindename = ifelse( is.na(Gemeindename), "Bezirk",Gemeindename),
          Gemeindename = ifelse(is.na(Bezirksname), "Kanton",Gemeindename),
          Gemeindename = ifelse(Bezirksname =="Genève" &  !Gemeindename =="Bezirk", "Genève",Gemeindename),
+         Gemeindename = ifelse(Bezirksname=="Appenzell-Innerrhoden","Bezirk", Gemeindename ),
          Fallzahl = ifelse(is.na(Fallzahl), 0, Fallzahl)) 
 
 
 
 dt2<- dt %>%
   dplyr:: mutate(iso_cw_y = as.factor(iso_cw_y),
-                 Gemeindename = as.factor(Gemeindename),
-                 Bezirksname = ifelse(is.na(Bezirksname),"Appenzell-Innerrhoden",Bezirksname ),
-                 `Bezirks-nummer` =ifelse(is.na(`Bezirks-nummer`), 1600, `Bezirks-nummer`)) %>%
+                 Gemeindename = as.factor(Gemeindename)) %>%
+                 # Bezirksname = ifelse(is.na(Bezirksname), "Appenzell-Innerrhoden",Bezirksname ),
+                 # `Bezirks-nummer` =ifelse(is.na(`Bezirks-nummer`), 1600, `Bezirks-nummer`)) %>%
   dplyr::filter(Gemeindename %in% c( "Bezirk")) %>%
   group_by(Bezirksname,iso_cw_y,`Bezirks-nummer`) %>%
   summarise(Fallzahl_t = sum(Fallzahl))
@@ -62,9 +60,10 @@ dt3 <- dt %>%
   distinct( `Bezirks-nummer`,iso_cw_y, .keep_all = TRUE)  %>%
   full_join(dt2) %>%
   filter(!is.na(Bezirksname)) %>%
-  mutate(
-         Cases = ifelse(Fallzahl_t>fallzahl_bezirk,Fallzahl_t, fallzahl_bezirk))
+  mutate(Cases = ifelse(Fallzahl_t>fallzahl_bezirk,Fallzahl_t, fallzahl_bezirk),
+         Kanton = ifelse(Bezirksname=="Appenzell-Innerrhoden","AI", Kanton ),
+         Kanton = ifelse(Bezirksname=="Genève","GE", Kanton ))
 
-write.xlsx(dt3 ,paste0("Faelle_Bezirk.xlsx"), row.names=FALSE, overwrite = TRUE)
+write.xlsx(dt3 ,"data/Faelle_Bezirke.xlsx", row.names=FALSE, overwrite = TRUE)
 
 
